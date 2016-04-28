aa41f400049d83b2576c0c67589eda203a23be28
pameljoel <pj1986@gmail.com>
2016-04-28 21:42:44 +0200

    yeoman scaffolding

---
+++ app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name lolAppApp
+ * @description
+ * # lolAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('lolAppApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lolAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lolAppApp
+ */
+angular.module('lolAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
