f0cb34c2bb5913ed2d01c8895dcd3fbca253eebe
pameljoel <enzo.cuoghi@virgilio.it>
2016-05-01 14:40:24 +0000

    homepage makes succesfull api call

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,20 @@
  * Controller of the lolAppApp
  */
 angular.module('lolAppApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope, $http) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    
+
+    $scope.submit = function(summonerName){
+      console.log('looking for summoner' + summonerName);
+        
+      $http.get('https://lane-gg.herokuapp.com/api/summoner/' + summonerName).then(function(result){
+          $scope.summoners = result.data;
+      });
+      
+    };
   });
