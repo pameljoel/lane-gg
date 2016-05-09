cbdf11fe8f32db6c6067b78e28f8b42c090b519b
pameljoel <enzo.cuoghi@virgilio.it>
2016-05-09 20:08:24 +0000

    spinner

--- app/scripts/controllers/results.js
+++ app/scripts/controllers/results.js
@@ -9,14 +9,18 @@
  */
 angular.module('lolAppApp')
   .controller('ResultsCtrl', function ($scope, $http, $routeParams) {
+    $scope.summonerId = $routeParams.id;
     
+    $scope.loading = true;
     $http.get('https://lane-gg.herokuapp.com/api/summoner/'+$routeParams.region + '/' + $routeParams.id + '/match')
       .then(function(result){
+        $scope.loading = false;
         $scope.match = result.data;
         $scope.oddsPercentage = 75;
       })
       .catch(function(error){
-        $scope.error = "Sorry we could'nt find an active game for this summoner";
+        $scope.error = "Sorry we couldn't find an active game for this summoner";
+        $scope.loading = false;
       });
     
     
